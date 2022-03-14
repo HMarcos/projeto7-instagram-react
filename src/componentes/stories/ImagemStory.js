71326408b3f8d0f073cfe37f5fa66b15f243c0c8
Marcos Henrique <marcosmarcone48@gmail.com>
2022-03-14 15:58:17 -0300

    Update ImagemStory.js

--- src/componentes/stories/ImagemStory.js
+++ src/componentes/stories/ImagemStory.js
@@ -1,7 +1,7 @@
 export default function ImagemStory(props) {
     return (
         <div className="story-bg-img">
-            <img className="story-background" src="imagens/stories_background.jpg" />
+            <img className="story-background" src="imagens/stories_background.jpg" alt="story background" />
             <a href="#"><img className="story-image" src={props.imagemSrc} alt={props.imagemAlt} /></a>
         </div>
     );
